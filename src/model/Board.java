0cc121a058e05159662f43476705e0f2a9bfeeb0
Gem Aludino <g.aludino@gmail.com>
2019-03-03 17:30:12 -0500

    Fixed toString. See lines 86 and 94 to turn on "release" mode grid

--- src/model/Board.java
+++ src/model/Board.java
@@ -22,25 +22,31 @@ import model.Piece.Color;
 public class Board {
 	
 	public class Cell {
+		private Position pos;
 		private Piece piece;
 		private boolean occupied;
 
-		public Cell(Piece piece) {
+		public Cell(Position pos) {
+			this.pos = pos;
+			piece = null;
+			occupied = false;
+		}
+		
+		public void assignPiece(Piece piece) {
 			this.piece = piece;
-			
-			occupied = piece != null ? true : false;
+			occupied = true;
 		}
 		
 		@Override
 		public String toString() {
 			String str = "";
 			
-			String placeholder = "";
+			String placeholder = "--";
 			
 			// determine if a placeholder is "##" or " "
 			// by field pos.
-
 			
+	
 			str = occupied ? piece.toString() : placeholder;
 			
 			return str;
@@ -54,35 +60,39 @@ public class Board {
 		
 		for (int file = 0; file < cell.length; file++) {
 			for (int rank = 0; rank < cell[file].length; rank++) {
-				Cell newCell = new Cell(new Pawn(Color.BLACK, new Position(0, 0)));
-				
+				Cell newCell = new Cell(new Position(file, rank));	
 				cell[file][rank] = newCell;
 			}
-		
 		}
-	
+		
+		// not sure if a cell should be aware of position,
+		// or piece should be aware of position, or both.
+		
+		// Test instantiation of a black pawn at 0,0 (not the right place)
+		// but needed it to test toString of Board
+		cell[3][1].assignPiece(new Pawn(Color.BLACK, new Position(0, 0)));
 	}
 	
 	@Override
 	public String toString() {
 		String str = "";
-		
-		final int CELL_LENGTH = cell.length;
-		
-		for (int file = 0; file < CELL_LENGTH; file++) {
-			for (int rank = cell[file].length - 1; rank >= 0; rank--) {
+
+		for (int rank = cell.length - 1; rank >= 0; rank--) {
+			for (int file = 0; file < cell[rank].length; file++) {
 				str += cell[file][rank];
 				str += " ";
 				
-				if (rank == 0) {
-					str += String.format("%d", CELL_LENGTH - file);
+				if (file == cell[rank].length - 1) {
+					//str += String.format("%d", rank + 1);	// FOR RELEASE
+					str += String.format("%d", rank);
 				}
 			}
-			
 			str += "\n";
 		}
+
 		
-		str += String.format(" a  b  c  d  e  f  g  h\n");
+		//str += String.format(" a  b  c  d  e  f  g  h\n");	// FOR RELEASE
+		str += String.format(" 0  1  2  3  4  5  6  7\n");
 		
 		return str;
 	}
