ef2a7adfd689adbaec75a75a104048b149171e33
Gem Aludino <g.aludino@gmail.com>
2019-03-05 00:34:01 -0500

    Encapsulated all pieces into a PieceSet. A Board will have 2 instances.

--- src/model/PieceSet.java
+++ src/model/PieceSet.java
@@ -20,20 +20,64 @@ import model.Piece.Color;
  *
  */
 public class PieceSet {
-
-
 	
 	public enum PieceType {
 		KING, QUEEN, BISHOP_L, BISHOP_R, KNIGHT_L, KNIGHT_R, ROOK_L, ROOK_R,
 		PAWN_0, PAWN_1, PAWN_2, PAWN_3, PAWN_4, PAWN_5, PAWN_6, PAWN_7
 	};
 	
-
+	private int PIECE_COUNT = 16;
+	
+	private Color color;
+	protected Piece[] piece;
 		
-
+	/**
+	 * 
+	 * @param color
+	 */
+	public PieceSet(Color color) {
+		this.color = color;
 		
+		piece = new Piece[PIECE_COUNT];
+		
+		piece[PieceType.KING.ordinal()] = new King(color);
+		piece[PieceType.QUEEN.ordinal()] = new Queen(color);
+		piece[PieceType.BISHOP_L.ordinal()] = new Bishop(color);
+		piece[PieceType.BISHOP_R.ordinal()] = new Bishop(color);
+		piece[PieceType.KNIGHT_L.ordinal()] = new Knight(color);
+		piece[PieceType.KNIGHT_R.ordinal()] = new Knight(color);
+		piece[PieceType.ROOK_L.ordinal()] = new Rook(color);
+		piece[PieceType.ROOK_R.ordinal()] = new Rook(color);
+		piece[PieceType.PAWN_0.ordinal()] = new Pawn(color);
+		piece[PieceType.PAWN_1.ordinal()] = new Pawn(color);
+		piece[PieceType.PAWN_2.ordinal()] = new Pawn(color);
+		piece[PieceType.PAWN_3.ordinal()] = new Pawn(color);
+		piece[PieceType.PAWN_4.ordinal()] = new Pawn(color);
+		piece[PieceType.PAWN_5.ordinal()] = new Pawn(color);
+		piece[PieceType.PAWN_6.ordinal()] = new Pawn(color);
+		piece[PieceType.PAWN_7.ordinal()] = new Pawn(color);
+	}
 	
+	/**
+	 * 
+	 * @return
+	 */
+	public Color getPieceSetColor() {
+		return color;
+	}
 	
-
-	
+	/**
+	 * 
+	 * @param pos
+	 * @return
+	 */
+	public Piece getPiece(Position pos) {
+		for (int i = 0; i < piece.length; i++) {
+			if (piece[i].pos.equals(pos)) {
+				return piece[i];
+			}
+		}
+		
+		return null;
+	}
 }
