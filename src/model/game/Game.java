ad122754ce16fedea13f5d736d985f09bb8c15d1
Gem Aludino <g.aludino@gmail.com>
2019-03-26 23:00:51 -0400

    Updated documentation

--- src/model/game/Game.java
+++ src/model/game/Game.java
@@ -206,6 +206,9 @@ public class Game {
 		return result;
 	}
 
+	/**
+	 * Begins game loop
+	 */
 	public void start() {
 		int[] fileRankArray = null;
 
--- src/model/game/Player.java
+++ src/model/game/Player.java
@@ -22,10 +22,10 @@ import model.chess_set.PieceSet;
  * @author gemuelealudino
  */
 class Player {
-	
+
 	private PieceType.Color color;
 	private PieceSet pieceSet;
-	
+
 	/**
 	 * Parameterized constructor
 	 * 
@@ -35,7 +35,7 @@ class Player {
 		this.color = color;
 		pieceSet = null;
 	}
-	
+
 	/**
 	 * Assign a PieceSet to a Player given a Board
 	 * 
@@ -45,49 +45,51 @@ class Player {
 	 */
 	boolean assignPieceSet(Board board) {
 		boolean result = false;
-		
+
 		if (pieceSet == null) {
 			PieceSet whiteSet = board.getWhiteSet();
 			PieceSet blackSet = board.getBlackSet();
-			
+
 			boolean playerIsWhite = color.equals(whiteSet.getPieceSetColor());
-					
+
 			pieceSet = playerIsWhite ? whiteSet : blackSet;
-			
+
 			result = true;
 		}
-		
+
 		return result;
 	}
 
 	/**
 	 * Player makes a request to play a move
 	 * 
-	 * @param board the current Board instance used during a match
+	 * @param board         the current Board instance used during a match
 	 * @param piecePosition the Position of a chosen Piece
-	 * @param newPosition the Position desired by the Player for a chosen Piece
-     * @param promo	  integer that represents the piece to promote to (if != -1)
+	 * @param newPosition   the Position desired by the Player for a chosen
+	 *                      Piece
+	 * @param promo         integer that represents the piece to promote to (if
+	 *                      != -1)
 	 * 
 	 * @return true if successful, false otherwise
 	 */
-	boolean playMove(Board board, Position piecePosition, 
-			Position newPosition, int promo) {
+	boolean playMove(Board board, Position piecePosition, Position newPosition,
+			int promo) {
 		boolean result = false;
-		
-		boolean requestDiffersFromNewPosition = 
-				(piecePosition.equals(newPosition) == false);
-		
+
+		boolean requestDiffersFromNewPosition = (piecePosition
+				.equals(newPosition) == false);
+
 		if (requestDiffersFromNewPosition) {
 			Piece pieceRequested = pieceSet.getPieceByPosition(piecePosition);
-			
+
 			if (pieceRequested == null) {
 				String error = String.format(
-						"ERROR: No %s piece at position %s exists.", 
-						color, piecePosition);
+						"ERROR: No %s piece at position %s exists.", color,
+						piecePosition);
 				System.err.println(error);
 			} else {
 				PieceType promoType = null;
-				
+
 				switch (promo) {
 				case 1:
 					promoType = PieceType.QUEEN;
@@ -100,29 +102,20 @@ class Player {
 					break;
 				case 7:
 					promoType = PieceType.ROOK_R;
-					break;					
+					break;
 				}
-				
-				result = board.movePiece(pieceRequested, pieceSet, 
-						newPosition, promoType);
 
-				/*
-				 *  Not necessary, because pieceRequested
-				 *  is being passed to board.movePiece(Piece piece, Pos..),
-				 *  and movePiece() calls piece.move()
-				 *  
-				if (result) {
-					pieceRequested.setPosition(newPosition);					
-				}
-				*/
-				
-				//System.out.println(pieceSet);
+				result = board.movePiece(pieceRequested, pieceSet, newPosition,
+						promoType);
 			}
 		}
-		
+
 		return result;
 	}
-	
+
+	/**
+	 * Prints the Player's pieceSet
+	 */
 	public void printPieceSet() {
 		System.out.println(pieceSet);
 	}
